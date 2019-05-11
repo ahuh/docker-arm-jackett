f416db67d1fbc48f449771cf94234d0929672059
ahuh <ahuh@free.fr>
2019-05-11 11:30:49 +0200

    Allow update using Web UI

--- jackett/start.sh
+++ jackett/start.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-JACKETT_LAUNCHER="mono /opt/jackett/JackettConsole.exe --NoUpdates"
+#JACKETT_LAUNCHER="mono /opt/jackett/JackettConsole.exe --NoUpdates"
+JACKETT_LAUNCHER="mono /opt/jackett/JackettConsole.exe"
 
 . /etc/jackett/updateJackett.sh
 
